65971c598b6dac770c315cb6b05fd6f404262945
tezeb <tezeb+github@outoftheblue.pl>
2016-10-16 05:52:37 +0200

    py solution for c14

--- py_common/cryptoUtils.py
+++ py_common/cryptoUtils.py
@@ -73,10 +73,108 @@ def encryptAES_CBC(plain, key, iv):
 def createRandomString(length, alphabet=string.printable):
     return ''.join(random.SystemRandom().choice(alphabet) for _ in range(length)).encode('ascii')
 
-def isECB(cryptxt, blockLen=16):
+def isECB(cryptxt, blockLen=16, retChunk=False):
     (count, chunk) = findMaxRepetitionsCnt(
             [cryptxt[i:i+blockLen] for i in range(0, len(cryptxt)-blockLen+1, blockLen)]
             )
+    if retChunk:
+        return (count > 1, chunk)
     if count > 1:
         return True
     return False
+
+def bfEcbByte(oracle, decoded, blockLen, marker, encMarker, needle, alphabet=string.printable):
+    for c in alphabet:
+        maybeNeedle = marker + decoded[-blockLen+1:]
+        maybeNeedle.append(ord(c))
+        maybeNeedle.append(ord('X'))
+        c2 = oracle.encrypt(maybeNeedle * blockLen)
+        offset = c2.find(encMarker)
+        c2 = c2[offset+len(encMarker):offset+len(encMarker)+blockLen]
+        if c2 == needle:
+            decoded.append(ord(c))
+            return True
+    return False
+
+def decryptEcbMode(oracle, blockLen, expectedCryptxLen, marker, encMarker, decoded = bytearray()):
+    if len(decoded) == 0:
+        decoded = bytearray([ord("C") for _ in range(blockLen-1)])
+    fillLen = blockLen-1
+
+    tries = 0
+    while (len(decoded) - fillLen) < expectedCryptxLen:
+        print("\rDecrypted:",len(decoded)-fillLen,"/",expectedCryptxLen,end="")
+        #   fillLen does not count into block count
+        blockNmbr = (len(decoded)-fillLen)//blockLen
+        #   prefixLen means missing characters for current block
+        #   so it must decrease from blockLen-1 -> 0 for each block
+        #   fillLen also have to be subtracted
+        #   and it happens that blockLen - 1 == fillLen
+        prefixLen = (fillLen * 2 - len(decoded))%blockLen
+        test = marker + decoded[:prefixLen]
+        offset = -1
+        ltries = 0
+        while offset < 0:
+            c1 = oracle.encrypt(test)
+            offset = c1.find(encMarker)
+            ltries+=1
+            if ltries%100 == 0:
+                test.insert(0, ord("X"))
+        tries+=ltries
+        #   the moment we will start decoding padding it will be aligned
+        #   to 0x01(by our prefixLen) so we will get proper expectedCryptxLen
+        #   and exacly 1byte of padding to decrypt(== 0x01)
+        expectedCryptxLen = min(expectedCryptxLen, len(c1) - offset - len(encMarker) - prefixLen)
+        needleOff = offset+len(encMarker)+blockNmbr*blockLen
+        c1 = c1[needleOff:needleOff+blockLen]
+        if not bfEcbByte(oracle, decoded, blockLen, marker, encMarker, c1):
+            #   !!! WARNING !!!
+            #   after hitting first padding byte, decoding further bytes requires
+            #   one to increment already decrypted bytes of padding for each step
+            #   as this is dynamically created by oracle for each data to be encrypted
+            #   so it changes with each new byte under decryption
+            #   This may be enabled by uncommenting padding, else statement
+            #   and disabling expectedCryptxLen calculation above
+            #
+            padding = '\x01'
+#           padding = ''.join(chr(i) for i in range(1, blockLen))
+            if not bfEcbByte(oracle, decoded, blockLen, marker, encMarker, c1, alphabet=padding):
+                print("\n[-] No match in alphabet(including padding bytes)\n")
+                print("Use following snippet to continue:")
+                print("key = unhexlify('",hexlify(oracle.key),"')")
+                print("marker = unhexlify('",hexlify(marker),"')")
+                print("encMarker = unhexlify('",hexlify(encMarker),"')")
+                print("decoded = ",repr(decoded))
+                print("o = service(key=key)")
+                print("decryptEcbMode(o, 16, 139, marker, encMarker, decoded=decoded)")
+                sys.exit(1)
+            else:
+                pass
+#                print("[+] Binary match, probably padding")
+#                for i in range(decoded[-1]):
+#                    decoded[-(i+1)] += 1
+    print("\n[>] Avg tries:",tries/(len(decoded)-fillLen))
+    print("[>] Decoded:")
+    print(decoded[fillLen:-decoded[-1]].decode('latin-1'))
+
+def discoverBlockLen(oracle, probes=32, minBlockLen=16, interactive=True):
+    test = bytearray()
+    cryptLen = set()
+    blockLen = 0
+    while True:
+        for _ in range(probes):
+            cryptLen.add(len(oracle.encrypt(bytes(test))))
+            test.append(ord('A'))
+        l = sorted(cryptLen)
+        blockLen = l[-1]-l[0]
+        p = l[0]
+        for i in l:
+            j = i - p
+            if j > 0 and j < blockLen:
+                blockLen = i - p
+            p = i
+        if interactive:
+            print("[>] BlockLen guess:",blockLen)
+        if blockLen == minBlockLen or (verbose and input("Try harder? [y/N]") != "y"):
+            break
+    return blockLen
---
+++ s2/c14.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+
+import py_common.cryptoUtils as cu
+import base64
+import string
+import sys
+from random import SystemRandom as SRand
+from py_common.utils import findMaxRepetitionsCnt
+from binascii import hexlify, unhexlify
+
+class service:
+    def __init__(self, key=cu.createRandomString(16), prefixLen=2048):
+        self.key = key
+        self.secret = "Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK"
+        self.prefixLen = prefixLen
+
+    def encrypt(self, plain):
+        data = cu.createRandomString(SRand().randint(0, self.prefixLen))
+        data += plain
+        data += base64.b64decode(self.secret)
+        data = cu.pad(data, 16)
+#        print("E:",len(data), hexlify(data))
+        return cu.encryptAES_ECB(data, self.key)
+
+def main():
+    o = service(prefixLen=256)
+    test = bytearray()
+    blockLen = cu.discoverBlockLen(o)
+    #   markers for finding our cryptxt after encryption
+    #   use of two different two protect against randomness prepending "A"
+    marker1 = bytearray(b"A" * blockLen)
+    marker2 = bytearray(b"B" * blockLen)
+    print("[>] Block len:",blockLen)
+    test = marker1 * 8
+    cryptxt = o.encrypt(test)
+    (ecb_a, aChunk) = cu.isECB(cryptxt, retChunk=True)
+    test = marker2 * 8
+    cryptxt = o.encrypt(test)
+    (ecb_b, bChunk) = cu.isECB(cryptxt, retChunk=True)
+    if not ecb_a or not ecb_b:
+        print("[-] Non ECB oracle")
+        sys.exit(1)
+    print("[+] ECB mode discovered")
+
+    #   at most 31bytes longer then the real cryptxt
+    cryptxtLen = len(cryptxt) - cryptxt.rfind(bChunk) - blockLen 
+    
+    cu.decryptEcbMode(o, blockLen, cryptxtLen, marker1 + marker2, aChunk + bChunk)
+
+if __name__ == "__main__":
+    main()
